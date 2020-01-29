2c1f8e2ed6574e756546c087b38d83c4e58f640a
svidovich <samuel.vidovich@gmail.com>
2020-01-28 20:22:17 -0500

    add script + readme

---
+++ README.md
@@ -0,0 +1,1 @@
+I couldn't find any record of all of the dependencies for Ardour, a DAW for linux. Here's an install script for debian distros to install them.
---
+++ install-deps.sh
@@ -0,0 +1,22 @@
+sudo apt install libsndfile-dev \
+libgtkmm-3.0-dev \
+libgtkmm-2.4 \
+libcurl4-gnutls-dev \
+libarchive-dev \
+libtag1-dev \
+vamp-plugin-sdk \
+librubberband-dev \
+libfftw3-dev \
+libaubio-dev \
+libusb-1.0.0-dev \
+clang \
+libcwiid-dev \
+libjack0 \
+liblrdf0-dev \
+libsamplerate0-dev \
+lv2-dev \
+libserd-dev \
+libsord-dev \
+libsratom-dev \
+liblilv-dev \
+libsuil-dev
